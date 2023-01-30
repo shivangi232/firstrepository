0e596a1fc2d7cfced0b59142028738768ce7b5cc
unknown <shivangigeitpl@gmail.com>
2023-01-30 17:38:14 +0530

    again copy

--- first.py
+++ first.py
@@ -4,3 +4,7 @@ from django.db import models
 class Image(models.Model):
  photo = models.ImageField(upload_to="myimage")
  date = models.DateTimeField(auto_now_add=True)
+ 
+ class Image(models.Model):
+     photo = models.ImageField(upload_to="myimage")
+ date = models.DateTimeField(auto_now_add=True)
